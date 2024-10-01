da4cdcf272a7ffd3b3f1cc7ecb33e4b953e4bf92
dcremi <remi.de.chalendar@gmail.com>
2024-10-01 17:34:42 +0200

    Folder management

--- src/extractJsComment.ts
+++ src/extractJsComment.ts
@@ -9,44 +9,50 @@ import {
 import { GenericCommentBlock, GenericGlobalComments } from "./ressources/interfaces";
 const fs = require("fs");
 
-// const inputFilePath = "./sampleData/test.js"
-const inputFileName = "contextSetup.ts";
-const inputFilePath = `./input/${inputFileName}`;
-const intermediateOutputFilePath = "./output/jsCommentExtractedOutput";
-const finalOutputFilePath = `./output/${inputFileName}Output`;
+const folderPath = "input";
+fs.readdir(folderPath, (err, files) => {
+	files.forEach((fileName) => {
+		const inputFilePath = `./input/${fileName}`;
+		const intermediateOutputFilePath = `./output/intermediate/${fileName}Intermediate`;
+		const finalOutputFilePath = `./output/${fileName}Output`;
 
-// #region run
-/** STEP 1 : Get file content */
-const file = fs.readFileSync(inputFilePath, { encoding: "utf-8" });
+		// #region run
+		/** STEP 1 : Get file content */
+		const file = fs.readFileSync(inputFilePath, { encoding: "utf-8" });
 
-/** STEP 2 : Retrieve only js doc style comment between in blocks */
-const jsCommentsBlocks = extractJsComContent(file);
+		/** STEP 2 : Retrieve only js doc style comment between in blocks */
+		const jsCommentsBlocks = extractJsComContent(file);
 
-/** STEP 3 : Remove start and end indicator of blocks */
-const jsCommentsBlocksCleaned: string[] = [];
-jsCommentsBlocks?.forEach((element: string, index: number) => {
-	jsCommentsBlocksCleaned[index] = removeJsComBoundary(element);
+		/** STEP 3 : Remove start and end indicator of blocks */
+		const jsCommentsBlocksCleaned: string[] = [];
+		jsCommentsBlocks?.forEach((element: string, index: number) => {
+			jsCommentsBlocksCleaned[index] = removeJsComBoundary(element);
+		});
+
+		/** STEP 4 : For each comments block extract tag value and content */
+		const genericCommentBlocks: GenericCommentBlock[] = [];
+		const genericGlobalComments: GenericGlobalComments = {
+			genericCommentBlocks
+		};
+		jsCommentsBlocksCleaned.forEach((element, index) => {
+			genericGlobalComments.genericCommentBlocks[index] = {
+				blocNumber: index + 1,
+				genericTagSentences: getTagDataFromBlock(element, getTagIndex(element))
+			};
+		});
+
+		/** STEP 5 : Write intermediary json to file */
+		JSONToFile(genericGlobalComments, intermediateOutputFilePath);
+
+		/** STEP 6 :  extract tag specific data */
+		const finalJson = extractTagSpecificData(fileName, genericGlobalComments);
+
+		/** STEP 7 : Write final json to file */
+		JSONToFile(finalJson, finalOutputFilePath);
+		// #endregion
+
+		// const file2 = fs.readFileSync(`${intermediateOutputFilePath}.json`, { encoding: "utf-8" });
+		// const jsondatatest = JSON.parse(file2)
+		// const finalJson = extractTagSpecificData(inputFileName, jsondatatest);
+	});
 });
-
-/** STEP 4 : For each comments block extract tag value and content */
-const genericCommentBlocks: GenericCommentBlock[] = [];
-const genericGlobalComments: GenericGlobalComments = {
-	genericCommentBlocks
-};
-jsCommentsBlocksCleaned.forEach((element, index) => {
-	genericGlobalComments.genericCommentBlocks[index] = {
-		blocNumber: index + 1,
-		genericTagSentences: getTagDataFromBlock(element, getTagIndex(element))
-	};
-});
-
-/** STEP 5 : Write intermediary json to file */
-JSONToFile(genericGlobalComments, intermediateOutputFilePath);
-
-/** STEP 6 :  extract tag specific data */
-const finalJson = extractTagSpecificData(inputFileName, genericGlobalComments);
-
-/** STEP 7 : Write final json to file */
-JSONToFile(finalJson, finalOutputFilePath);
-
-// #endregion
