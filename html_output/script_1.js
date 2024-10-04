015b23a03c4ed15ca22bd3fa6be5b55d84765463
dcremi <remi.de.chalendar@gmail.com>
2024-10-04 12:35:38 +0200

    rename

--- html_output/script_1.js
+++ html_output/script_1.js
@@ -1,15 +1,8 @@
-import nodePandoc from 'node-pandoc'
-let src = './word.docx';
-
-// Arguments can be either a single String or in an Array
-let args = '-f docx -t markdown -o ./markdown.md';
-
-
 document.addEventListener("DOMContentLoaded", function () {
 	fetch("json_1.json", { mode: "no-cors" })
 		.then((response) => response.json())
 		.then((data) => {
-				const dataDisplay = document.getElementById("dataDisplay");
+			const dataDisplay = document.getElementById("dataDisplay");
 
 			// Create HTML elements to display the JSON data
 			const nameElement = document.createElement("p");
@@ -28,4 +21,3 @@ document.addEventListener("DOMContentLoaded", function () {
 		})
 		.catch((error) => console.error("Error fetching JSON data:", error));
 });
-
