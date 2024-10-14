62985b29c5594ff0b010dab04545b72b835fb650
dcremi <remi.de.chalendar@gmail.com>
2024-10-14 15:22:23 +0200

    ID-04 for each files

--- src/createHtmlStruct.ts
+++ src/createHtmlStruct.ts
@@ -25,35 +25,31 @@ copyFilesStructToHtml(filesPaths, sourcePathName, destinationPath);
 
 //#endregion
 
-const filePath = "./json_output/storageOutput.json";
-// filesPaths.forEach((filePath) => {
+// const filePath = "./json_output/storageOutput.json";
+filesPaths.forEach((filePath) => {
+	const fileJsonData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+	const commentJsonBlocks = fileJsonData.commentBlocks;
+	const interactionTypeJsonBlocks = fileJsonData.interactionTypes;
 
-const fileJsonData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
-const commentJsonBlocks = fileJsonData.commentBlocks;
-const interactionTypeJsonBlocks = fileJsonData.interactionTypes;
+	var fileHtmlBlocks: string = "";
 
-var fileHtmlBlocks: string = "";
+	const fileHtmlDesc = createHtmlFileDesc(fileJsonData);
+	fileHtmlBlocks = fileHtmlDesc;
 
-const fileHtmlDesc = createHtmlFileDesc(fileJsonData);
-fileHtmlBlocks = fileHtmlDesc;
+	const fileHtmlInteractionTypes = createHtmlInteractionType(interactionTypeJsonBlocks);
+	fileHtmlBlocks += "<br/>" + fileHtmlInteractionTypes;
+	fileHtmlBlocks += "<br/><h2>Steps</h2>";
 
-const fileHtmlInteractionTypes = createHtmlInteractionType(interactionTypeJsonBlocks);
-fileHtmlBlocks += "<br/>" + fileHtmlInteractionTypes;
-fileHtmlBlocks += "<br/><h2>Steps</h2>";
+	commentJsonBlocks.forEach((commentBlock: CommentBlock) => {
+		const defHtmlBlock = createDefHtmlBlock(commentBlock);
+		const paramHtmlBlock = createParamHtmlBlock(commentBlock);
+		const exampleHtmlBlock = createExampleHtmlBlock(commentBlock);
+		const todoHtmlBlock = createToDoHtmlBlock(commentBlock);
+		const bloc = createStepDefHtmlBlock(defHtmlBlock, paramHtmlBlock, exampleHtmlBlock, todoHtmlBlock);
+		fileHtmlBlocks += bloc + "<br/><br/>";
+	});
 
-commentJsonBlocks.forEach((commentBlock: CommentBlock) => {
-	const defHtmlBlock = createDefHtmlBlock(commentBlock);
-	const paramHtmlBlock = createParamHtmlBlock(commentBlock);
-	const exampleHtmlBlock = createExampleHtmlBlock(commentBlock);
-	const todoHtmlBlock = createToDoHtmlBlock(commentBlock);
-	const bloc = createStepDefHtmlBlock(defHtmlBlock, paramHtmlBlock, exampleHtmlBlock, todoHtmlBlock);
-	fileHtmlBlocks += bloc + "<br/><br/>";
+	const htmlFile = createHtmlFile(fileHtmlBlocks);
+
+	HtmlToFile(htmlFile, `html_output/html_pages/${path.basename(filePath).replace(path.extname(filePath), "")}`);
 });
-
-// console.log(commentHtmlBlocks);
-
-const htmlFile = createHtmlFile(fileHtmlBlocks);
-HtmlToFile(htmlFile, "html_output/index");
-
-// filepath
-// });
