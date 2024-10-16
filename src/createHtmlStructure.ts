512d27996151f77b972d663d4432b73869cba872
dcremi <remi.de.chalendar@gmail.com>
2024-10-16 16:57:47 +0200

    ID-06 - Rename level to reflect the level "home" 0

--- src/createHtmlStructure.ts
+++ src/createHtmlStructure.ts
@@ -8,12 +8,13 @@ const path = require("path");
 /* -------------------------------- Variables ------------------------------- */
 const htmlOutputFolder = "./html_output";
 const htmlFilesOutputFolder = path.join(htmlOutputFolder, "pages_tree");
-var Level2IndexLinks = "";
+var Level1IndexLinks = "";
 
 /* -------------------------------------------------------------------------- */
 /*                  STEP 1 - Read level definition json file                  */
 /* -------------------------------------------------------------------------- */
 const levelDefinitionData: Levels = JSON.parse(fs.readFileSync("input/levelDefinition.json", "utf-8"));
+
 if (!fs.existsSync(htmlFilesOutputFolder)) {
 	fs.mkdirSync(htmlFilesOutputFolder);
 }
@@ -27,22 +28,29 @@ levelDefinitionData.level_1s.forEach((level_1) => {
 	if (!fs.existsSync(level1FolderPath)) {
 		fs.mkdirSync(level1FolderPath);
 	}
-	const level2IndexFilePath = level1FolderPath + "/index";
+
+	const level1IndexFilePath = level1FolderPath + "/index";
+
 	/* ----------------------- Create level 2 files empty ----------------------- */
-	HtmlToFile("", level2IndexFilePath);
+	HtmlToFile("", level1IndexFilePath);
+
 	level_1.level_2s.forEach((level_2) => {
 		const level2FilePath = path.join(level1FolderPath, level_2.levelName);
 		HtmlToFile("", level2FilePath);
 	});
-
 	/* -------------- Create html links block to level 1 index.html ------------- */
-	Level2IndexLinks += `
+	Level1IndexLinks += `
 				<a href="/${level1FolderPath}/index.html">
 					<button type="button" class="btn btn-success btn-lg"> ${level_1.levelName} </button>
 				</a>
 				<br /><br />`;
 });
 
-/* ----------------- Create and write level 1 index.html ---------------- */
-const level1IndexFile = createLevel1IndexHtml(Level2IndexLinks);
-HtmlToFile(level1IndexFile, htmlFilesOutputFolder + "/index");
+/* ----------------- Create and write level 1 indexedDB.html ---------------- */
+const level0IndexFile = createLevel1IndexHtml(Level1IndexLinks);
+HtmlToFile(level0IndexFile, htmlFilesOutputFolder + "/index");
+
+/* -------------------------------------------------------------------------- */
+/*                   STEP 3 - Create each level 2 html file                   */
+/* -------------------------------------------------------------------------- */
+levelDefinitionData.level_1s.forEach((level_1) => {});
