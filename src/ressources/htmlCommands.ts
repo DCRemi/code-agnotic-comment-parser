1cfab0981f9bb2977e07164cc97ee99b726a2415
dcremi <remi.de.chalendar@gmail.com>
2025-01-29 19:48:34 +0100

    ID-09 remove double prism script

--- src/ressources/htmlCommands.ts
+++ src/ressources/htmlCommands.ts
@@ -335,7 +335,6 @@ export function createHtmlFile(mainHtml: string, level_2: Level_2): string {
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
 		<title>Home, Block</title>
 		<link type="text/css" rel="stylesheet" href="../../styles/style.css" />
-		<link type="text/css" rel="stylesheet" href="../../styles/prism.css" />		
 		<link
 			rel="stylesheet"
 			href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
